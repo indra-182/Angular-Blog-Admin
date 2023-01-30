cab9ce5ae1f1b3ef47fb523bc074d3e3cc06c59c
indra-182 <mailto:afebriansyah.vendor.pasti@pegadaian.co.id>
2023-01-30 14:25:57 +0700

    add post component

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.css']
+  styleUrls: ['./new-post.component.css'],
 })
 export class NewPostComponent implements OnInit {
+  permalink: string = '';
+  imgSrc: any =
+    'https://www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg';
+  selectedImg: any;
+  categories: Array<object>;
 
-  constructor() { }
+  constructor(private categoriesService: CategoriesService) {}
 
   ngOnInit(): void {
+    this.categoriesService.loadData().subscribe((val) => {
+      this.categories = val;
+    });
   }
 
+  onTitleChanged($event: any) {
+    const title = $event.target.value;
+    this.permalink = title.replace(/\s/g, '-');
+  }
+
+  showPreview($event: any) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imgSrc = e.target.result;
+    };
+    reader.readAsDataURL($event.target.files[0]);
+    this.selectedImg = $event.target.files[0];
+  }
 }
